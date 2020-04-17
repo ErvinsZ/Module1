f7d65372b11395e9a5c11d392120ec35c8f47783
ErvinsZ <63841322+ErvinsZ@users.noreply.github.com>
2020-04-17 14:39:51 +0300

    Update Day4.js

--- Day4.js
+++ Day4.js
@@ -1,19 +1,33 @@
-//STRINGS
-/*
-let mynumber = 2
 
-const myString = `First line
-Second line
-This is my value ${mynumber}
-`
+//DAY 4 Homework
 
-console.log(myString);
-
+/* EXERCISE 1
+Write a function "area" which receives 2 parameters (l1,l2) and calculate the area of the rectangle.
 */
 
-// const myString = "Whatever" 
-// myString.slice(3,6);
+const area = function(l1,l2){
+    return l1*l2
+}
+const areaOfRectangle = area(2,5);
+console.log("Rectangle area: ", areaOfRectangle);
 
-// myString.indexOf('a');
+/* EXERCISE 2
+Write a function "crazySum" which receives two given integers. If the two values are same, then returns triple their sum.
+*/
 
-// console.log(myString);
+
+/* EXERCISE 3
+Write a function "crazyDiff" that computes the absolute difference between a given number and 19. 
+Returns triple their absolute difference if the specified number is greater than 19.
+*/
+
+const crazyDiff = function(x){
+    let crazyDiff = 27
+    if (crazyDiff>19) {
+        console.log("Crazy Difference is: ", (x-19)*3);
+    } else {
+        console.log("Crazy Difference is: ", x-19);
+    }
+}
+const finalDiff = crazyDiff(27);
+    
