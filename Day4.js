77628d8b545fd30ce59e5030cfa5e94d1adfe7c9
ErvinsZ <63841322+ErvinsZ@users.noreply.github.com>
2020-04-17 12:02:59 +0300

    Test1

---
+++ Day4.js
@@ -0,0 +1,19 @@
+//STRINGS
+/*
+let mynumber = 2
+
+const myString = `First line
+Second line
+This is my value ${mynumber}
+`
+
+console.log(myString);
+
+*/
+
+// const myString = "Whatever" 
+// myString.slice(3,6);
+
+// myString.indexOf('a');
+
+// console.log(myString);
