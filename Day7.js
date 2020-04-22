82f6c3ac84568277c0894d11fb03d0b2eafa1a58
ErvinsZ <63841322+ErvinsZ@users.noreply.github.com>
2020-04-22 14:11:59 +0300

    Day7

---
+++ Day7.js
@@ -0,0 +1,13 @@
+const x = "value2";
+
+switch (x) {
+    case "value1":
+        console.log('Something');
+        break;
+    case "value2":
+        console.log("value 2 Branch")
+        break;
+        default:
+            console.log('None of the previous');
+            break;
+}
